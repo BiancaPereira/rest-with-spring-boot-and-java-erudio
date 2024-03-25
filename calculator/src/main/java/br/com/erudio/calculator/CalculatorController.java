ed9004548b11d8f72ad80cc7334fa3c9361df2fc
Bianca Pereira <heybiibis@gmail.com>
2024-03-25 20:55:41 -0300

    Adiciona atividade (resto das operações matemáticas)

--- calculator/src/main/java/br/com/erudio/calculator/CalculatorController.java
+++ calculator/src/main/java/br/com/erudio/calculator/CalculatorController.java
@@ -12,12 +12,71 @@ public class CalculatorController {
     public Double sum(
         @PathVariable(value = "numberOne") String numberOne,
         @PathVariable(value = "numberTwo") String numberTwo
-    ) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-            throw new UnsupportedMathOperationException("Por favor, use um valor numérico.");
-        }
+    ) {
+        exceptionResponse(numberOne, numberTwo);
 
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}")
+    public Double subtraction(
+        @PathVariable(value = "numberOne") String numberOne,
+        @PathVariable(value = "numberTwo") String numberTwo
+    ) {
+        exceptionResponse(numberOne, numberTwo);
+
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}")
+    public Double multiplication(
+        @PathVariable(value = "numberOne") String numberOne,
+        @PathVariable(value = "numberTwo") String numberTwo
+    ) {
+        exceptionResponse(numberOne, numberTwo);
+
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/division/{numberOne}/{numberTwo}")
+    public Double division(
+        @PathVariable(value = "numberOne") String numberOne,
+        @PathVariable(value = "numberTwo") String numberTwo
+    ) {
+        exceptionResponse(numberOne, numberTwo);
+
+        if (convertToDouble(numberTwo) == 0) {
+            throw new UnsupportedMathOperationException("Não se pode dividir por zero.");
+        }
+
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/average/{numberOne}/{numberTwo}")
+    public Double average(
+        @PathVariable(value = "numberOne") String numberOne,
+        @PathVariable(value = "numberTwo") String numberTwo
+    ) {
+        exceptionResponse(numberOne, numberTwo);
+
+        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+    }
+
+    @RequestMapping(value = "/square/{numberOne}")
+    public Double square(
+        @PathVariable(value = "numberOne") String numberOne
+    ) {
+        if (!isNumeric(numberOne)) {
+            throw new UnsupportedMathOperationException("Por favor, digite um valor numérico.");
+        }
+
+        return convertToDouble(numberOne) * convertToDouble(numberOne);
+    }
+
+    private void exceptionResponse(String numberOne, String numberTwo) {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Por favor, digite um valor numérico.");
+        }
     }
 
     private Double convertToDouble(String strNumber) {
